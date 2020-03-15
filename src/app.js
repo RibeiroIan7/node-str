a0a2446dc2dd591017410d406791bd3d3fb6d611
ribeiroian7 <you@example.com>
2020-03-15 13:38:09 -0300

    CRUD REST (CRUD e REST)

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 'use strict'
 
+//Importando 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const router = express.Router();
+
+//configurando utilização bodyParser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //configurando rota
 const route = router.get('/', (req, res, next) => {
@@ -13,6 +19,22 @@ const route = router.get('/', (req, res, next) => {
     });
 });
 
+const create = router.post('/', (req, res, next) => {
+    res.status(201).send(req.body);
+});
+
+const put = router.put('/:id', (req, res, next) => {
+    let id = req.params.id;
+    res.status(201).send({ id: id, item: req.body });
+});
+
+const del = router.delete('/', (req, res, next) => {
+    res.status(200).send(req.body);
+});
+
 app.use('/', route);
+app.use('/products', create);
+app.use('/products', put);
+app.use('/products', del);
 
 module.exports = app;
